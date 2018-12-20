8761e5fbdedcc3fde20153c9804c5fe0532c92a0
jrecio <recio.juan@gmail.com>
2018-12-20 18:07:35 +0100

    heroku n+1

--- server/app.js
+++ server/app.js
@@ -31,6 +31,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(`${__dirname}/public`))
 
 app.use(cors({
   credentials: true,
